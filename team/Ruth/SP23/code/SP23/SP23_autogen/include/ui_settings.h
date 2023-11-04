810d0053ed0dc0b50d23087af7a0faf018d67a6a
Ruthy-coder <ruthamosu@gmail.com>
2023-11-04 20:01:30 +0100

    Test

--- team/Ruth/SP23/code/SP23/SP23_autogen/include/ui_settings.h
+++ team/Ruth/SP23/code/SP23/SP23_autogen/include/ui_settings.h
@@ -11,8 +11,11 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QRadioButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -21,8 +24,12 @@ QT_BEGIN_NAMESPACE
 class Ui_Settings
 {
 public:
-    QMenuBar *menubar;
     QWidget *centralwidget;
+    QRadioButton *radioButton;
+    QPushButton *pushButton;
+    QWidget *gridLayoutWidget;
+    QGridLayout *gridLayout;
+    QMenuBar *menubar;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *Settings)
@@ -30,12 +37,25 @@ public:
         if (Settings->objectName().isEmpty())
             Settings->setObjectName("Settings");
         Settings->resize(800, 600);
-        menubar = new QMenuBar(Settings);
-        menubar->setObjectName("menubar");
-        Settings->setMenuBar(menubar);
         centralwidget = new QWidget(Settings);
         centralwidget->setObjectName("centralwidget");
+        radioButton = new QRadioButton(centralwidget);
+        radioButton->setObjectName("radioButton");
+        radioButton->setGeometry(QRect(190, 100, 91, 22));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(280, 230, 80, 24));
+        gridLayoutWidget = new QWidget(centralwidget);
+        gridLayoutWidget->setObjectName("gridLayoutWidget");
+        gridLayoutWidget->setGeometry(QRect(70, 20, 160, 80));
+        gridLayout = new QGridLayout(gridLayoutWidget);
+        gridLayout->setObjectName("gridLayout");
+        gridLayout->setContentsMargins(0, 0, 0, 0);
         Settings->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(Settings);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 800, 21));
+        Settings->setMenuBar(menubar);
         statusbar = new QStatusBar(Settings);
         statusbar->setObjectName("statusbar");
         Settings->setStatusBar(statusbar);
@@ -48,6 +68,8 @@ public:
     void retranslateUi(QMainWindow *Settings)
     {
         Settings->setWindowTitle(QCoreApplication::translate("Settings", "MainWindow", nullptr));
+        radioButton->setText(QCoreApplication::translate("Settings", "RadioButton", nullptr));
+        pushButton->setText(QCoreApplication::translate("Settings", "PushButton", nullptr));
     } // retranslateUi
 
 };
