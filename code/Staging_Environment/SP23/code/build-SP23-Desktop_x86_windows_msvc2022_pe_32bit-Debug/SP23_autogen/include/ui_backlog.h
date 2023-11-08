5d3e7adfac79efdf30f5923a3a734d3b3f596225
AidaZZ <128605556+AidaZZ@users.noreply.github.com>
2023-11-08 21:03:55 +0100

    Update
    Deleted Level and To Do buttons in the backlog

--- code/Staging_Environment/SP23/code/build-SP23-Desktop_x86_windows_msvc2022_pe_32bit-Debug/SP23_autogen/include/ui_backlog.h
+++ code/Staging_Environment/SP23/code/build-SP23-Desktop_x86_windows_msvc2022_pe_32bit-Debug/SP23_autogen/include/ui_backlog.h
@@ -11,7 +11,6 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QComboBox>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
@@ -21,8 +20,6 @@
 #include <QtWidgets/QTableView>
 #include <QtWidgets/QTableWidget>
 #include <QtWidgets/QTextBrowser>
-#include <QtWidgets/QTextEdit>
-#include <QtWidgets/QToolButton>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -31,15 +28,12 @@ class Ui_Backlog
 {
 public:
     QWidget *centralwidget;
-    QComboBox *comboBox;
     QPushButton *pushButton_2;
     QPushButton *pushButton;
     QTableView *tableView_9;
     QTableWidget *tableWidget;
     QTextBrowser *textBrowser_2;
-    QTextEdit *textEdit_3;
     QLineEdit *lineEdit;
-    QToolButton *toolButton;
     QTextBrowser *textBrowser_3;
     QMenuBar *menubar;
     QStatusBar *statusbar;
@@ -51,10 +45,6 @@ public:
         Backlog->resize(800, 600);
         centralwidget = new QWidget(Backlog);
         centralwidget->setObjectName("centralwidget");
-        comboBox = new QComboBox(centralwidget);
-        comboBox->addItem(QString());
-        comboBox->setObjectName("comboBox");
-        comboBox->setGeometry(QRect(630, 200, 71, 21));
         pushButton_2 = new QPushButton(centralwidget);
         pushButton_2->setObjectName("pushButton_2");
         pushButton_2->setGeometry(QRect(590, 30, 141, 51));
@@ -94,28 +84,19 @@ public:
         textBrowser_2->setGeometry(QRect(240, 120, 301, 61));
         textBrowser_2->setAutoFillBackground(false);
         textBrowser_2->setStyleSheet(QString::fromUtf8("background-color: rgba(255, 255, 255, 0%)"));
-        textEdit_3 = new QTextEdit(centralwidget);
-        textEdit_3->setObjectName("textEdit_3");
-        textEdit_3->setGeometry(QRect(510, 200, 104, 21));
         lineEdit = new QLineEdit(centralwidget);
         lineEdit->setObjectName("lineEdit");
         lineEdit->setGeometry(QRect(60, 200, 211, 22));
-        toolButton = new QToolButton(centralwidget);
-        toolButton->setObjectName("toolButton");
-        toolButton->setGeometry(QRect(590, 250, 161, 41));
         textBrowser_3 = new QTextBrowser(centralwidget);
         textBrowser_3->setObjectName("textBrowser_3");
         textBrowser_3->setGeometry(QRect(50, 40, 91, 41));
         Backlog->setCentralWidget(centralwidget);
         tableView_9->raise();
-        comboBox->raise();
         pushButton_2->raise();
         pushButton->raise();
         tableWidget->raise();
         textBrowser_2->raise();
-        textEdit_3->raise();
         lineEdit->raise();
-        toolButton->raise();
         textBrowser_3->raise();
         menubar = new QMenuBar(Backlog);
         menubar->setObjectName("menubar");
@@ -133,8 +114,6 @@ public:
     void retranslateUi(QMainWindow *Backlog)
     {
         Backlog->setWindowTitle(QCoreApplication::translate("Backlog", "MainWindow", nullptr));
-        comboBox->setItemText(0, QCoreApplication::translate("Backlog", "To Do", nullptr));
-
         pushButton_2->setText(QCoreApplication::translate("Backlog", "Back", nullptr));
         pushButton->setText(QCoreApplication::translate("Backlog", "Create Issue", nullptr));
         QTableWidgetItem *___qtablewidgetitem = tableWidget->verticalHeaderItem(0);
@@ -165,16 +144,7 @@ public:
 "li.checked::marker { content: \"\\2612\"; }\n"
 "</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:24pt; font-weight:700; color:#000000;\">Product Backlog</span></p></body></html>", nullptr));
-        textEdit_3->setHtml(QCoreApplication::translate("Backlog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"hr { height: 1px; border-width: 0; }\n"
-"li.unchecked::marker { content: \"\\2610\"; }\n"
-"li.checked::marker { content: \"\\2612\"; }\n"
-"</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Label</p></body></html>", nullptr));
         lineEdit->setText(QCoreApplication::translate("Backlog", "Issue Title 1 - can be a short sentence", nullptr));
-        toolButton->setText(QCoreApplication::translate("Backlog", "can be used for drop down", nullptr));
         textBrowser_3->setHtml(QCoreApplication::translate("Backlog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
