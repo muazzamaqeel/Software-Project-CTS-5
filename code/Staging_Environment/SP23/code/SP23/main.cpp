c812fc73e0415c1c20504ee35403476f32049f6a
Muazzam Bin Aqeel <128594910+muazzamaqeel@users.noreply.github.com>
2023-11-14 18:24:15 +0100

    Database Connection Update
    I have commented the code so that the other team members don't face any errors with the code, as they should first install the QSQL Drivers (Which i will tell them soon)

--- code/Staging_Environment/SP23/code/SP23/main.cpp
+++ code/Staging_Environment/SP23/code/SP23/main.cpp
@@ -1,9 +1,44 @@
 #include "mainwindow.h"
 //#include "registration_window.h"
 #include <QApplication>
+#include <QPixmap>
+#include "mainwindow.h"
+#include <QDebug>
+#include <QtSql/QSqlDatabase>
+#include <QDebug>
+#include <iostream>
+#include <QtSql/QSqlError>
+
+
 
 int main(int argc, char *argv[])
 {
+
+    /*
+    QString dbHostName = "aws-dbtest.cjqugotdygrg.eu-central-1.rds.amazonaws.com";
+    QString dbName = "scrummy";
+    QString dbUserName = "admin";
+    QString dbPassword = "sofproj23";
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName(dbHostName);
+    db.setDatabaseName(dbName);
+    db.setUserName(dbUserName);
+    db.setPassword(dbPassword);
+
+    if (db.open()) {
+        qDebug() << "Connected to the database!";
+        std::cout << "Connected!" << std::endl; // Output "Hello, World!" and a newline
+
+        // Perform database operations here
+        // ...
+        db.close(); // Close the connection when done
+    } else {
+        qDebug() << "Failed to connect. Error:" << db.lastError().text();
+    }
+
+    */
+
     QApplication app(argc, argv);
     //Creating an object of MainWindow to access its attributes
     MainWindow w;
