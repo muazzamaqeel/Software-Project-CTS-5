745f4c997e22764df5732cde0c357df67680694a
Muazzam Bin Aqeel <128594910+muazzamaqeel@users.noreply.github.com>
2023-11-18 12:02:07 +0100

    Sprint (Parent-Board) Adjustment
    Assisting Aida in her task

--- code/Staging_Environment/Recovered/Staging_Environment_Recorverd/parentboard.cpp
+++ code/Staging_Environment/Recovered/Staging_Environment_Recorverd/parentboard.cpp
@@ -1,8 +1,8 @@
 #include "parentboard.h"
 #include "ui_parentboard.h"
 
-#include "settings.h"
 #include "mainwindow.h"
+#include "sprints.h"
 
 parentboard::parentboard(QWidget *parent) :
     QWidget(parent),
@@ -19,31 +19,60 @@ parentboard::parentboard(QWidget *parent) :
 
 }
 
-parentboard::~parentboard()
-{
-    delete ui;
-}
-
-void parentboard::on_taskboardButton_clicked()
-{
-    ui->stackedWidget->setCurrentIndex(0);
-}
-
-void parentboard::on_backlogButton_clicked()
-{
-    ui->stackedWidget->setCurrentIndex(1);
-}
-
-void parentboard::on_sprintsButton_clicked()
-{
-        ui->stackedWidget->setCurrentIndex(2);
-
-
+    parentboard::~parentboard()
+    {
+        delete ui;
+    }
+    
+    void parentboard::on_taskboardButton_clicked()
+    {
+        ui->stackedWidget->setCurrentIndex(0);
+    }
+    
+    void parentboard::on_backlogButton_clicked()
+    {
+        ui->stackedWidget->setCurrentIndex(1);
+    }
+    
+    void parentboard::on_sprintsButton_clicked()
+    {
+            ui->stackedWidget->setCurrentIndex(2);
 
 
 
 
-}
+        ///Muazzam
+        ///In order to open a window on top of another window
+        // Sprints *sprintsWindow = new Sprints(this); // Assuming Sprint is a QDialog or QWidget
+        // Set the sprint window as a modal to block interaction with parentboard window
+        // sprintsWindow->setWindowModality(Qt::WindowModal);
+        // Show the sprint window
+        //sprintsWindow->show();
+    
+    }
+
+
+
+    ///Cosmin
+
+    // Settings button might not make it to the other windows besides MainWindow
+    //void parentboard::openSettings()
+    //{
+
+    //        close();
+    //        Settings* settingWindow = new Settings;
+    //        settingWindow -> showMaximized();
+
+    //}
+
+    //void parentboard::goBackToParentBoard()
+    //{
+    //        hide(); // Hide the settings window
+    //        parentboard* parentBoard = new parentboard;
+    //        parentBoard->showMaximized();
+    //}
+
+    //to be replaced with the Projects window
 
 void parentboard::on_confluenceButton_clicked()
 {
@@ -51,29 +80,16 @@ void parentboard::on_confluenceButton_clicked()
 }
 
 
-// Cosmin
 
-// Settings button might not make it to the other windows besides MainWindow
-//void parentboard::openSettings()
-//{
-
-//        close();
-//        Settings* settingWindow = new Settings;
-//        settingWindow -> showMaximized();
-
-//}
-
-//void parentboard::goBackToParentBoard()
-//{
-//        hide(); // Hide the settings window
-//        parentboard* parentBoard = new parentboard;
-//        parentBoard->showMaximized();
-//}
-
-//to be replaced with the Projects window
 void parentboard::goBackToMainWindow()
 {
         close(); // Hide the ParentBoard window
         MainWindow* mainWindow = new MainWindow; // Create a new instance of the main window
         mainWindow->showMaximized(); // Show the main window
 }
+
+
+
+
+
+
