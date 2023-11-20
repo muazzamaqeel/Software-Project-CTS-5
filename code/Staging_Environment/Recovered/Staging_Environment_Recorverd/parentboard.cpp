b7e018e486ae5069a573c72efd8567a6e1be27d4
Ruthy-coder <ruthamosu@gmail.com>
2023-11-21 00:07:46 +0100

    creation of userstories and task

--- code/Staging_Environment/Recovered/Staging_Environment_Recorverd/parentboard.cpp
+++ code/Staging_Environment/Recovered/Staging_Environment_Recorverd/parentboard.cpp
@@ -22,8 +22,19 @@ parentboard::parentboard(QWidget *parent) :
     ui->sprint_table->setColumnCount(2);
     ui->sprint_table->setHorizontalHeaderLabels({"Task Name", "Description"}); // Set column headers
 
+    // Set table for Userstories
+    ui->user_stories->setColumnCount(2);
+    ui->user_stories->setHorizontalHeaderLabels({"Userstory", "Description"}); // Set column headers
+
+    // Set table for Userstories
+    ui->issues->setColumnCount(2);
+    ui->issues->setHorizontalHeaderLabels({"Issue", "Description"}); // Set column headers
+
     connect(ui->exitButton, SIGNAL(clicked()), this, SLOT(goBackToMainWindow()));
     connect(ui->create_task_button, SIGNAL(clicked()), this, SLOT(on_createtask_sprint_clicked()));
+    connect(ui->button_userstory, SIGNAL(clicked()), this, SLOT(on_createuserstories_backlog_clicked()));
+    connect(ui->buttton_issue, SIGNAL(clicked()), this, SLOT(on_createissues_clicked()));
+
 }
 
 parentboard::~parentboard()
@@ -40,11 +51,13 @@ parentboard::~parentboard()
     void parentboard::on_taskboardButton_clicked()
     {
         ui->stackedWidget->setCurrentIndex(0);
+        const int numOfRowsToAdd = 3; // Change this number as per your requirement
     }
     
-    void parentboard::on_backlogButton_clicked()
+   void parentboard::on_backlogButton_clicked()
     {
-        ui->stackedWidget->setCurrentIndex(1);
+      ui->stackedWidget->setCurrentIndex(1);
+       const int numOfRowsToAdd = 3; // Change this number as per your requirement
     }
 
     void parentboard::on_sprintsButton_clicked()
@@ -81,8 +94,53 @@ parentboard::~parentboard()
     }
 
 
+    void parentboard::on_createuserstories_backlog_clicked() {
+        qDebug() << "Create task sprint button clicked.";
+
+        QString taskName = QInputDialog::getText(this, "Enter Userstory", "Userstory:");
+        QString taskDescription = QInputDialog::getText(this, "Enter Userstory", "Userstory Description:");
+
+        addBacklog(taskName, taskDescription);
+
+        qDebug() << "Userstory: " << taskName;
+        qDebug() << "Userstory Description: " << taskDescription;
+    }
 
 
+    void parentboard::addBacklog(const QString& taskName, const QString& description) {
+        int row = ui->user_stories->rowCount(); // Get the current row count
+        ui->user_stories->insertRow(row); // Insert a new row at the end
+
+        QTableWidgetItem *nameItem = new QTableWidgetItem(taskName);
+        QTableWidgetItem *descriptionItem = new QTableWidgetItem(description);
+
+        ui->user_stories->setItem(row, 0, nameItem); // Set task name in the first column
+        ui->user_stories->setItem(row, 1, descriptionItem); // Set description in the second column
+    }
+
+    void parentboard::on_createissues_clicked() {
+        qDebug() << "Create task sprint button clicked.";
+
+        QString taskName = QInputDialog::getText(this, "Enter Issue", "Issue:");
+        QString taskDescription = QInputDialog::getText(this, "Enter Issue Description", "Issue Description:");
+
+        addIssues(taskName, taskDescription);
+
+        qDebug() << "Issue: " << taskName;
+        qDebug() << "Issue Description: " << taskDescription;
+    }
+
+
+    void parentboard::addIssues(const QString& taskName, const QString& description) {
+        int row = ui->issues->rowCount(); // Get the current row count
+        ui->issues->insertRow(row); // Insert a new row at the end
+
+        QTableWidgetItem *nameItem = new QTableWidgetItem(taskName);
+        QTableWidgetItem *descriptionItem = new QTableWidgetItem(description);
+
+        ui->issues->setItem(row, 0, nameItem); // Set task name in the first column
+        ui->issues->setItem(row, 1, descriptionItem); // Set description in the second column
+    }
 
 void parentboard::on_confluenceButton_clicked()
 {
