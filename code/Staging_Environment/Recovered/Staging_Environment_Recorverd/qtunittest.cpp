e6f50dd44e0455ef7d76a22fd67a59c9cd7ff43a
AidaZZ <128605556+AidaZZ@users.noreply.github.com>
2023-12-07 11:45:49 +0100

    Structure - QTTest

--- code/Staging_Environment/Recovered/Staging_Environment_Recorverd/qtunittest.cpp
+++ code/Staging_Environment/Recovered/Staging_Environment_Recorverd/qtunittest.cpp
@@ -3,6 +3,23 @@
 #include <QtTest/QTest>
 #include "mainwindow.h"
 //#include "adminstrator.h"
+//#include "backlog.h"
+//#include "calendar.h"
+//#include "confluence.h"
+//#include "issuecreation.h"
+//#include "launchscreen.h"
+//#include "parentboard.h"
+//#include "projectcreation.h"
+//#include "projectsadmin.h"
+//#include "registration_window.h"
+//#include "settings.h"
+//#include "sprints.h"
+//#include "taskboard.h"
+//#include "pb_confluence_implemenation.h"
+//#include "pb_productbacklog_implementation.h"
+//#include "pb_sprint_implemenation.h"
+//#include "pb_taskboard_implemenation.h"
+//#include "pb_team_implemenation.h"
 
 
 
@@ -29,8 +46,99 @@ void QTUnitTest::MainWindowTest() {
 }
 
 
-//Implementation of the Tests of the Adminstrator.cpp
+//Implementation of the Tests of the adminstrator.cpp
 void QTUnitTest::AdminstratorTest(){
 
 }
 
+//Implementation of the Tests of the backlog.cpp
+void QTUnitTest::BacklogTest(){
+
+}
+
+//Implementation of the Tests of the calendar.cpp
+void QTUnitTest::CalendarTest(){
+
+}
+
+//Implementation of the Tests of the confluence.cpp
+void QTUnitTest::ConfluenceTest() {
+
+}
+
+//Implementation of the Tests of the issuecreation.cpp
+void QTUnitTest::IssuecreationTest(){
+
+}
+
+//Implementation of the Tests of the launchscreen.cpp
+void QTUnitTest::LaunchscreenTest(){
+
+}
+
+//Implementation of the Tests of the parentboard.cpp
+void QTUnitTest::Parentboardtest(){
+
+}
+
+//Implementation of the Tests of the pb_confluence_implementation.cpp
+void QTUnitTest::Pb_confluence_implementationTest(){
+
+}
+
+
+//Implementation of the Tests of the pb_productbacklog_implementation.cpp
+void QTUnitTest::Pb_productbacklog_implementationTest(){
+
+}
+
+
+//Implementation of the Tests of the pb_sprint_implementation.cpp
+void QTUnitTest::Pb_sprint_implemenationTest(){
+
+}
+
+//Implementation of the Tests of the pb_taskboard_implementation.cpp
+void QTUnitTest::Pb_taskboard_implemenationTest(){
+
+}
+
+
+//Implementation of the Tests of the pb_team_implementation.cpp
+void QTUnitTest::Pb_team_implemenationTest(){
+
+}
+
+
+//Implementation of the Tests of the projectcreation.cpp
+void QTUnitTest::ProjectcreationTest(){
+
+}
+
+
+//Implementation of the Tests of the projectsadmin.cpp
+void QTUnitTest::ProjectsadminTest(){
+
+}
+
+
+//Implementation of the Tests of the registration_window.cpp
+void QTUnitTest::Registration_windowTest(){
+
+}
+
+//Implementation of the Tests of the settings.cpp
+void QTUnitTest::SettingsTest(){
+
+}
+
+
+//Implementation of the Tests of the sprints.cpp
+void QTUnitTest::SprintsTest(){
+
+}
+
+//Implementation of the Tests of the taskboard.cpp
+void QTUnitTest::TaskboardTest(){
+
+}
